c869ce1cadbeaa7880328c5263e69d60b341ea78
unknown <Breke64@gmail.com>
2023-11-29 19:48:53 -0500

    continued work on homeowkr but really stuck on launching server to begin testing routes

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,63 @@
+const { User, Thought  } = require('../models');
+
+// const { ObjectId } = require('mongoose').Types;
+// const { Student, Course } = require('../models');
+
+
+module.exports = {
+  async getThoughts(req, res){
+    try{
+      const thought = await Thought.find();
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getSingleThought(req, res){
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId }).select('-__v');
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this ID'});
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      res.json(thought);
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json(err);
+    }
+  },
+  async deleteThought(req, res) {
+    try {
+      const thought = await Thought.findoneAndDelete({ _id: req.params.thoughtId});
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this ID'});
+      }
+      await Thought.deleteMany({ _id: { $in: thought.user }});
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+      );
+      if (!thought) {
+        return res.status(404).json({ message: ' No thought with this ID!'});
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
+
+
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,38 @@
 const router = require('express').Router();
-// const {}
+
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+} = require ('../../controllers/userControllers');
+
+// get  all users // also create user?
+router.route('/').get(getUsers).post(createUser);
 
 
-router.route('/').get()
+// get single user by id.
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+
+
+
+
+
+// delete a user by id bonus to remove associated thoughtswhen deleted.
+router.route('./:userId').delete(deleteUser);
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
